c40cee108db285f2c5bd0491e13e71e5c86a70ff
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2020-02-20 14:10:19 +0100

    Add Last Will support; reformat code

--- include/mqtt.hpp
+++ include/mqtt.hpp
@@ -1,34 +1,36 @@
 #pragma once
 
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/event_groups.h>
-#include <esp_log.h>
-#include <mqtt_client.h>
 #include <functional>
 #include <map>
+#include <mqtt_client.h>
 
 #include "logger.hpp"
 
 class MQTTClient {
-    static Logger LOGGER;
-    static constexpr auto CONNECTED_BIT = BIT0;
+	static Logger LOGGER;
+	static constexpr auto CONNECTED_BIT = BIT0;
 
 public:
-    static std::error_code initialize(const std::string &client_id, const std::string &broker_address, TickType_t timeout);
-    static std::error_code publish(const std::string &topic, const std::string &data, bool retain = false);
-    static std::error_code subscribe(const std::string &topic);
-    static void add_connection_callback(std::function<void()> callback);
-    static void add_callback(const std::string &topic, std::function<void (const std::string &, const std::string &)> callback);
+	static std::error_code initialize(const std::string &client_id, const std::string &broker_address,
+	                                  const std::string &last_will_topic, TickType_t timeout);
+	static std::error_code publish(const std::string &topic, const std::string &data, bool retain = false);
+	static std::error_code subscribe(const std::string &topic);
+	static void add_connection_callback(std::function<void()> callback);
+	static void add_callback(const std::string &topic,
+	                         std::function<void(const std::string &, const std::string &)> callback);
 
-    static bool is_connected() {
-        return (xEventGroupGetBitsFromISR(_event_group) & CONNECTED_BIT) != (EventBits_t)0;
-    }
+	static bool is_connected() {
+		return (xEventGroupGetBitsFromISR(_event_group) & CONNECTED_BIT) != (EventBits_t)0;
+	}
 
 private:
-    static esp_mqtt_client_handle_t _client;
-    static EventGroupHandle_t _event_group;
-    static std::function<void()> _on_connection;
-    static std::multimap<std::string, std::function<void (const std::string &, const std::string &)>> _callbacks;
+	static esp_mqtt_client_handle_t _client;
+	static EventGroupHandle_t _event_group;
+	static std::function<void()> _on_connection;
+	static std::multimap<std::string, std::function<void(const std::string &, const std::string &)>> _callbacks;
 
-    static esp_err_t _event_handler(esp_mqtt_event_handle_t event);
+	static esp_err_t _event_handler(esp_mqtt_event_handle_t event);
 };
--- mqtt.cpp
+++ mqtt.cpp
@@ -5,16 +5,16 @@ Logger MQTTClient::LOGGER("MQTTClient");
 esp_mqtt_client_handle_t MQTTClient::_client;
 EventGroupHandle_t MQTTClient::_event_group;
 std::function<void()> MQTTClient::_on_connection;
-std::multimap<std::string, std::function<void (const std::string &, const std::string &)>> MQTTClient::_callbacks;
+std::multimap<std::string, std::function<void(const std::string &, const std::string &)>> MQTTClient::_callbacks;
 
-
-std::error_code MQTTClient::initialize(const std::string &client_id, const std::string &broker_address, TickType_t timeout) {
+std::error_code MQTTClient::initialize(const std::string &client_id, const std::string &broker_address,
+                                       const std::string &last_will_topic, TickType_t timeout) {
 	LOGGER.debug("Initializing");
 	esp_log_level_set("MQTT_CLIENT", ESP_LOG_NONE);
 
 	_event_group = xEventGroupCreate();
 
-	esp_mqtt_client_config_t mqtt_cfg = {};
+	esp_mqtt_client_config_t mqtt_cfg = {.lwt_topic = last_will_topic.c_str(), .lwt_msg = "offline", .lwt_qos = 1};
 	mqtt_cfg.event_handle = _event_handler;
 	mqtt_cfg.uri = broker_address.c_str();
 	mqtt_cfg.client_id = client_id.c_str();
@@ -26,8 +26,8 @@ std::error_code MQTTClient::initialize(const std::string &client_id, const std::
 	}
 
 	LOGGER.info("Starting client");
-	RETURN_IF_ERROR( esp_mqtt_client_start(_client) );
-	
+	RETURN_IF_ERROR(esp_mqtt_client_start(_client));
+
 	if ((xEventGroupWaitBits(_event_group, CONNECTED_BIT, true, true, timeout) & CONNECTED_BIT) == 0) {
 		return RuntimeError::MQTTInitializationFailed;
 	}
@@ -36,7 +36,7 @@ std::error_code MQTTClient::initialize(const std::string &client_id, const std::
 }
 
 std::error_code MQTTClient::publish(const std::string &topic, const std::string &data, bool retain) {
-	if (esp_mqtt_client_publish(_client, topic.c_str(), data.c_str(), data.size(), 1,  static_cast<int>(retain)) == -1) {
+	if (esp_mqtt_client_publish(_client, topic.c_str(), data.c_str(), data.size(), 1, static_cast<int>(retain)) == -1) {
 		return RuntimeError::MQTTPublishFailed;
 	}
 
@@ -44,7 +44,7 @@ std::error_code MQTTClient::publish(const std::string &topic, const std::string 
 }
 
 std::error_code MQTTClient::subscribe(const std::string &topic) {
-	if(esp_mqtt_client_subscribe(_client, topic.c_str(), 1) == -1) {
+	if (esp_mqtt_client_subscribe(_client, topic.c_str(), 1) == -1) {
 		return RuntimeError::MQTTSubscribeFailed;
 	}
 	return {};
@@ -54,62 +54,63 @@ void MQTTClient::add_connection_callback(std::function<void()> callback) {
 	_on_connection = std::move(callback);
 }
 
-void MQTTClient::add_callback(const std::string &topic, std::function<void (const std::string &, const std::string &)> callback) {
+void MQTTClient::add_callback(const std::string &topic,
+                              std::function<void(const std::string &, const std::string &)> callback) {
 	_callbacks.emplace(topic, callback);
 }
 
-esp_err_t MQTTClient::_event_handler(esp_mqtt_event_handle_t event)  {
+esp_err_t MQTTClient::_event_handler(esp_mqtt_event_handle_t event) {
 	auto client = event->client;
 	// your_context_t *context = event->context;
 	switch (event->event_id) {
-		case MQTT_EVENT_CONNECTED: {
-			LOGGER.info("MQTT_EVENT_CONNECTED");
-			xEventGroupSetBits(_event_group, CONNECTED_BIT);
-			if (_on_connection) {
-				_on_connection();
-			}
-			break;
+	case MQTT_EVENT_CONNECTED: {
+		LOGGER.info("MQTT_EVENT_CONNECTED");
+		xEventGroupSetBits(_event_group, CONNECTED_BIT);
+		if (_on_connection) {
+			_on_connection();
 		}
-		case MQTT_EVENT_DISCONNECTED: {
-			LOGGER.info("MQTT_EVENT_DISCONNECTED");
-			xEventGroupClearBits(_event_group, CONNECTED_BIT);
-			esp_mqtt_client_start(_client);
-			break;
-		}
+		break;
+	}
+	case MQTT_EVENT_DISCONNECTED: {
+		LOGGER.info("MQTT_EVENT_DISCONNECTED");
+		xEventGroupClearBits(_event_group, CONNECTED_BIT);
+		esp_mqtt_client_start(_client);
+		break;
+	}
 
-		case MQTT_EVENT_SUBSCRIBED: {
-			LOGGER.info("MQTT_EVENT_SUBSCRIBED, msg_id={}", event->msg_id);
-			break;
+	case MQTT_EVENT_SUBSCRIBED: {
+		LOGGER.info("MQTT_EVENT_SUBSCRIBED, msg_id={}", event->msg_id);
+		break;
+	}
+	case MQTT_EVENT_UNSUBSCRIBED: {
+		LOGGER.info("MQTT_EVENT_UNSUBSCRIBED, msg_id={}", event->msg_id);
+		break;
+	}
+	case MQTT_EVENT_PUBLISHED: {
+		LOGGER.info("MQTT_EVENT_PUBLISHED, msg_id={}", event->msg_id);
+		break;
+	}
+	case MQTT_EVENT_DATA: {
+		std::string topic(event->topic, 0, event->topic_len);
+		std::string data(event->data, 0, event->data_len);
+		LOGGER.info("MQTT_EVENT_DATA");
+		LOGGER.debug("TOPIC=" + topic);
+		LOGGER.debug("DATA=" + data);
+		auto matches = _callbacks.equal_range(topic);
+		for (auto it = matches.first; it != matches.second; ++it) {
+			LOGGER.debug("callback match");
+			it->second(topic, data);
 		}
-		case MQTT_EVENT_UNSUBSCRIBED: {
-			LOGGER.info("MQTT_EVENT_UNSUBSCRIBED, msg_id={}", event->msg_id);
-			break;
-		}
-		case MQTT_EVENT_PUBLISHED: {
-			LOGGER.info("MQTT_EVENT_PUBLISHED, msg_id={}", event->msg_id);
-			break;
-		}
-		case MQTT_EVENT_DATA: {
-			std::string topic(event->topic, 0, event->topic_len);
-			std::string data(event->data, 0, event->data_len);
-			LOGGER.info("MQTT_EVENT_DATA");
-			LOGGER.debug("TOPIC=" + topic);
-			LOGGER.debug("DATA=" + data);
-			auto matches = _callbacks.equal_range(topic);
-			for (auto it = matches.first; it != matches.second; ++it) {
-				LOGGER.debug("callback match");
-				it->second(topic, data);
-			}
-			break;
-		}
-		case MQTT_EVENT_ERROR: {
-			LOGGER.info("MQTT_EVENT_ERROR");
-			break;
-		}
-		default: {
-			LOGGER.info("Other event id: {}", event->event_id);
-			break;
-		}
+		break;
+	}
+	case MQTT_EVENT_ERROR: {
+		LOGGER.info("MQTT_EVENT_ERROR");
+		break;
+	}
+	default: {
+		LOGGER.info("Other event id: {}", event->event_id);
+		break;
+	}
 	}
 	return ESP_OK;
 }
