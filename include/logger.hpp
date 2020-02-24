81011ec65f333de39c41815e659a8479a935bfa2
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2020-02-24 14:11:32 +0100

    Improve log_if_error function; reformat code

--- include/logger.hpp
+++ include/logger.hpp
@@ -1,52 +1,42 @@
 #pragma once
 
-#include <iostream>
 #include <fmt/format.h>
+#include <iostream>
 
-enum class LogLevel {
-    Debug,
-    Info,
-    Warning,
-    Error,
-    None
-};
+enum class LogLevel { Debug, Info, Warning, Error, None };
 
 class Logger {
-    static LogLevel global_log_level;
+	static LogLevel global_log_level;
 
 public:
-    static void initialize(LogLevel log_level);
+	static void initialize(LogLevel log_level);
 
-    Logger(const std::string &name);
-    
-    template <typename... Ts>
-    void debug(const Ts & ...args) {
-        if (global_log_level == LogLevel::Debug) {
-            std::cout << _tag << " DEBUG: " << fmt::format(args...) << "\n";
-        }
-    }
-    
-    template <typename... Ts>
-    void info(const Ts & ...args) {
-        if (global_log_level <= LogLevel::Info) {
-            std::cout << _tag << " INFO: " << fmt::format(args...) << "\n";
-        }
-    }
-    
-    template <typename... Ts>
-    void warning(const Ts & ...args) {
-        if (global_log_level <= LogLevel::Warning) {
-            std::cout << _tag << " WARNING: " << fmt::format(args...) << "\n";
-        }
-    }
-    
-    template <typename... Ts>
-    void error(const Ts & ...args) {
-        if (global_log_level <= LogLevel::Error) {
-            std::cout << _tag << " ERROR: " << fmt::format(args...) << "\n";
-        }
-    }
+	Logger(const std::string &name);
+
+	template <typename... Ts> void debug(const Ts &... args) const {
+		if (global_log_level == LogLevel::Debug) {
+			std::cout << _tag << " DEBUG: " << fmt::format(args...) << "\n";
+		}
+	}
+
+	template <typename... Ts> void info(const Ts &... args) const {
+		if (global_log_level <= LogLevel::Info) {
+			std::cout << _tag << " INFO: " << fmt::format(args...) << "\n";
+		}
+	}
+
+	template <typename... Ts> void warning(const Ts &... args) const {
+		if (global_log_level <= LogLevel::Warning) {
+			std::cout << _tag << " WARNING: " << fmt::format(args...) << "\n";
+		}
+	}
+
+	template <typename... Ts> void error(const Ts &... args) const {
+		if (global_log_level <= LogLevel::Error) {
+			std::cout << _tag << " ERROR: " << fmt::format(args...) << "\n";
+		}
+	}
 
 private:
-    std::string _tag;
+	std::string _tag;
 };
--- include/utils.hpp
+++ include/utils.hpp
@@ -11,7 +11,7 @@
 
 #include "logger.hpp"
 
-inline std::optional<float> stof_check(const std::string& input) {
+inline std::optional<float> stof_check(const std::string &input) {
 	size_t good_bytes;
 	auto output = std::stof(input, &good_bytes);
 	if (good_bytes == input.size()) {
@@ -20,7 +20,7 @@ inline std::optional<float> stof_check(const std::string& input) {
 	return {};
 }
 
-inline std::optional<int> stoi_check(const std::string& input) {
+inline std::optional<int> stoi_check(const std::string &input) {
 	size_t good_bytes;
 	auto output = std::stoi(input, &good_bytes);
 	if (good_bytes == input.size()) {
@@ -29,32 +29,37 @@ inline std::optional<int> stoi_check(const std::string& input) {
 	return {};
 }
 
-inline void log_if_error(const std::error_code &ec, const std::string &message) {
-    static Logger logger("APP");
+inline void log_if_error(const std::error_code &ec, const std::string &message, const Logger &logger) {
+	if (ec) {
+		logger.error(message, ec.message());
+	}
+}
 
-    if (ec) {
-        logger.error(message, ec.message());
-    }
+inline void log_if_error(const std::error_code &ec, const std::string &message) {
+	static Logger logger("APP");
+
+	log_if_error(ec, message, logger);
 }
 
 class Prescaler {
 public:
-    Prescaler(float input_min, float input_max, float output_min, float output_max, float output_off) :
-        input_min_(input_min), input_max_(input_max),
-        output_min_(output_min), output_max_(output_max),
-        output_off_(output_off) {
-    }
-    float operator()(float input) const {
-        if (input == input_min_) {
-            return output_off_;
-        }
-        return (std::clamp(input, input_min_, input_max_) - input_min_) / (input_max_ - input_min_) *
-        (output_max_ - output_min_) + output_min_;
-    }
+	Prescaler(float input_min, float input_max, float output_min, float output_max, float output_off)
+	    : input_min_(input_min), input_max_(input_max), output_min_(output_min), output_max_(output_max),
+	      output_off_(output_off) {
+	}
+	float operator()(float input) const {
+		if (input == input_min_) {
+			return output_off_;
+		}
+		return (std::clamp(input, input_min_, input_max_) - input_min_) / (input_max_ - input_min_) *
+		           (output_max_ - output_min_) +
+		       output_min_;
+	}
+
 private:
-    const float input_min_;
-    const float input_max_;
-    const float output_min_;
-    const float output_max_;
-    const float output_off_;
+	const float input_min_;
+	const float input_max_;
+	const float output_min_;
+	const float output_max_;
+	const float output_off_;
 };
