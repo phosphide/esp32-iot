0028c274c2da4218ab17cc090785c1a327054402
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2020-10-31 19:48:44 +0100

    Rewrite a macro as a function

--- include/errors.hpp
+++ include/errors.hpp
@@ -3,44 +3,41 @@
 #include <iostream>
 #include <system_error>
 
-#include "esp_err.h"
+#include <esp_err.h>
+#include <esp_system.h>
 
+#define RETURN_IF_ERROR(function_result)                                                                               \
+	do {                                                                                                                 \
+		if (function_result != ESP_OK) {                                                                                   \
+			std::error_code ec = to_strong_error(function_result);                                                           \
+			return ec;                                                                                                       \
+		}                                                                                                                  \
+	} while (0)
 
-#define RETURN_IF_ERROR(function_result) \
-do { \
-  if (function_result != ESP_OK) { \
-    std::error_code ec = to_strong_error(function_result); \
-    return ec; \
-  } \
-} while (0)
-#define RESTART_IF_ERROR(ec) \
-do { \
-  if (ec) { \
-    std::cerr << "CRITICAL: " << ec.message() << " - RESTARTING" << std::endl; \
-    esp_restart(); \
-  } \
-} while (0)
+inline void RESTART_IF_ERROR(const std::error_code &ec) {
+	if (ec) {
+		std::cerr << "CRITICAL: " << ec.message() << " - RESTARTING" << std::endl;
+		esp_restart();
+	}
+}
 
 enum class ESPError : esp_err_t {};
 
 enum class RuntimeError {
-  MQTTInitializationFailed = 1,
-  MQTTPublishFailed = 2,
-  MQTTSubscribeFailed = 3,
+	MQTTInitializationFailed = 1,
+	MQTTPublishFailed = 2,
+	MQTTSubscribeFailed = 3,
 };
 
 inline ESPError to_strong_error(esp_err_t err) {
-    return static_cast<ESPError>(err);
+	return static_cast<ESPError>(err);
 }
 
-namespace std
-{
-  template <>
-  struct is_error_code_enum<ESPError> : true_type {};
+namespace std {
+template <> struct is_error_code_enum<ESPError> : true_type {};
 
-  template <>
-  struct is_error_code_enum<RuntimeError> : true_type {};
-}
- 
+template <> struct is_error_code_enum<RuntimeError> : true_type {};
+} // namespace std
+
 std::error_code make_error_code(ESPError);
 std::error_code make_error_code(RuntimeError);
